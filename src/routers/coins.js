a4baa2bb8453c3f9204a9a3b3bd66afba179529a
rajamrit-singh <rajamritsingh1996@gmail.com>
2023-07-06 12:28:28 -0700

    added routers for coins and users

---
+++ src/routers/coins.js
@@ -0,0 +1,31 @@
+const config = require('../../config');
+const coinsUtil = require('../controllers/coins');
+const express = require('express')
+
+const router = new express.Router();
+
+router.get('/coins', async (req, res) => {
+    try {
+        const coins = await coinsUtil.getCoins();
+        res.send(JSON.stringify(coins));
+    } catch(error) {
+        res.status(500).send(`${error}. Unable to get data`)
+    }
+    
+});
+
+router.get('/coin/:uuid', async (req, res) => {
+    const uuid = req.params.uuid;
+    try {
+        const coin = await coinsUtil.getCoin(uuid);
+        console.log(coin);
+        res.send(JSON.stringify(coin));
+    } catch(error) {
+        res.status(500).send(`${error}. Unable to get data`)
+    }
+});
+
+router.get('/coin/')
+
+module.exports = router;
+
